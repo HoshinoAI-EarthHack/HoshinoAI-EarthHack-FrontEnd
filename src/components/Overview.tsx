cc5a9b0d15d9f4517d954f1e91ed259e2de889cc
johnyojohn <68317292+johnyojohn@users.noreply.github.com>
2024-01-07 21:21:40 -0500

    :/

--- src/components/Overview.tsx
+++ src/components/Overview.tsx
@@ -101,30 +101,31 @@ export function Overview() {
                 </thead>
                 <tbody className="divide-y divide-gray-200">
                   {/* must revise */}
-                  {responseData ? (
-                    responseData.map((data) => (
-                      <tr key={data.id}>
-                        <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
-                          {data.id}
-                        </td>
-                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                          {data.ideatext}
-                        </td>
-                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                          {data.resources}
-                        </td>
+                  {responseData !== null ? (
+                    // responseData.map((data) => (
+                    //   <tr key={data.id}>
+                    //     <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+                    //       {data.id}
+                    //     </td>
+                    //     <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                    //       {data.ideatext}
+                    //     </td>
+                    //     <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                    //       {data.resources}
+                    //     </td>
 
-                        <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
-                          <a
-                            href="#"
-                            className="text-indigo-600 hover:text-indigo-900"
-                          >
-                            Edit
-                            <span className="sr-only">, {data.id}</span>
-                          </a>
-                        </td>
-                      </tr>
-                    ))
+                    //     <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                    //       <a
+                    //         href="#"
+                    //         className="text-indigo-600 hover:text-indigo-900"
+                    //       >
+                    //         Edit
+                    //         <span className="sr-only">, {data.id}</span>
+                    //       </a>
+                    //     </td>
+                    //   </tr>
+                    // ))
+                    <h1>doesn't work</h1>
                   ) : (
                     <h1> LOADING... </h1>
                   )}
